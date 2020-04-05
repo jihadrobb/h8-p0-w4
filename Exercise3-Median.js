4112ae1df2dce55168dc768ece492cce12fd340d
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-04-05 23:44:02 +0700

    Create Exercise3-Median.js

---
+++ Exercise3-Median.js
@@ -0,0 +1,25 @@
+// Exercise 3 Cari Median
+function cariMedian(arr) {
+    //sortir array
+    for (let i = 0; i < arr.length-1; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if(arr[i] > arr[j]){
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    if((arr.length+1)%2 === 0){
+        return arr[(arr.length-1)/2];
+    } else {
+        return (arr[arr.length/2] + arr[(arr.length/2)-1])/2;
+    }
+  }
+  
+  // TEST CASES
+  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+  console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+  console.log(cariMedian([1, 3, 3])); // 3
+  console.log(cariMedian([7, 7, 8, 8])); // 7.5

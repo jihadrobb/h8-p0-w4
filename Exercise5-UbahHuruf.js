4162b2e9a116272f85929ba89ceca927de354451
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-04-06 19:48:47 +0700

    Update Exercise5-UbahHuruf.js

--- Exercise5-UbahHuruf.js
+++ Exercise5-UbahHuruf.js
@@ -4,19 +4,19 @@ function ubahHuruf(kata) {
     var katabaru = '';
     for (let i = 0; i < kata.length; i++) {
         for (let j = 0; j < alphabet.length; j++) {
-            if(kata[i] === alphabet[j] && j === alphabet.length-1){ //khusus huruf terakhir
+            if (kata[i] === alphabet[j] && j === alphabet.length - 1) { //khusus huruf terakhir
                 katabaru += alphabet[0];
-            } else if (kata[i] === alphabet[j]){
-                katabaru += alphabet[j+1];
+            } else if (kata[i] === alphabet[j]) {
+                katabaru += alphabet[j + 1];
             }
         }
     }
     return katabaru;
-  }
-  
-  // TEST CASES
-  console.log(ubahHuruf('wowz')); // xpx
-  console.log(ubahHuruf('developer')); // efwfmpqfs
-  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
-  console.log(ubahHuruf('keren')); // lfsfo
-  console.log(ubahHuruf('semangat')); // tfnbohbu
+}
+
+// TEST CASES
+console.log(ubahHuruf('wowz')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu

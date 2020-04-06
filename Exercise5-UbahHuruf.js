6e9454aad07064d95443726af146c39f61d5af9f
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-04-06 13:46:44 +0700

    handle huruf z

--- Exercise5-UbahHuruf.js
+++ Exercise5-UbahHuruf.js
@@ -4,7 +4,7 @@ function ubahHuruf(kata) {
     var katabaru = '';
     for (let i = 0; i < kata.length; i++) {
         for (let j = 0; j < alphabet.length; j++) {
-            if(kata[i] === alphabet[j] && j === alphabet.length-1){
+            if(kata[i] === alphabet[j] && j === alphabet.length-1){ //khusus huruf terakhir
                 katabaru += alphabet[0];
             } else if (kata[i] === alphabet[j]){
                 katabaru += alphabet[j+1];
